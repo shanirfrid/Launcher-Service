60ee3fe10319eb3bfc4f56519a1438825d34d721
shanirfrid <48044128+shanirfrid@users.noreply.github.com>
2022-03-05 22:00:10 +0200

    Initial commit

---
+++ src/main/java/com/shanir/launcherservice/LauncherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shanir.launcherservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauncherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauncherServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanir/launcherservice/LauncherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanir.launcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LauncherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
